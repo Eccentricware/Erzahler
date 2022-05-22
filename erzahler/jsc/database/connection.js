8d113f65b568c7630a80c4b21836aee410567fd3
Joseph Peck <joe.r.peck@gmail.com>
2022-05-22 11:35:31 -0700

    Delete erzahler/jsc directory
    I don't want to track JS output at this time.

--- erzahler/jsc/database/connection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// const pgPromise = require('pg-promise')();
-// import Pool from 'pg';
-// import { DBCredentials, victorCredentials } from '../secrets/dbCredentials';
-// const connection: DBCredentials = victorCredentials;
-// export const erzahlerDB = new Pool(victorCredentials);
-// export const erzahlerDB = pgPromise(connection);
