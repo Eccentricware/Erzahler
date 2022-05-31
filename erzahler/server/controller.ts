6333f1c3e827b50f4557732bdbad38148a1009c7
Joseph Peck <joe.r.peck@gmail.com>
2022-05-31 00:14:32 -0700

    Sign up is working for all intended providers

--- erzahler/server/controller.ts
+++ erzahler/server/controller.ts
@@ -11,11 +11,9 @@ const port: number = 8000;
 
 erzhaler.use(cors());
 erzhaler.use(bodyParser.json());
-console.log('Before Initialize')
 initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-console.log('After initialize');
 
 const accountService = new AccountService();
 
@@ -25,46 +23,25 @@ erzhaler.get('/', (request, response) => {
 });
 
 erzhaler.post('/register-user', (request, response) => {
-  let { idToken, username, email, password } = request.body;
+  let { idToken, username } = request.body;
 
-  accountService.addUserWithEmail(idToken, username, email)
+  accountService.attemptAddUserToDatabase(idToken, username)
     .then((result: any) => {
       response.send(result);
-
     })
     .catch((error: Error) => {
       response.send(error.message);
     });
 });
 
-erzhaler.get('/api/email-available-check/:email', (request, response) => {
-  const { email } = request.params;
-  accountService.checkEmailAvailability(email)
-    .then((emailAvailable: any) => {
-      response.send(emailAvailable);
-    })
-    .catch((e: Error) => console.error(e.stack));
-});
-
-erzhaler.post('/api/sign-in-with-google', (request, response) => {
-  // accountService.signInWithGoogle()
-  //   .then((user: any) => {
-  //     console.log('Final response user:', user);
-  //     response.send(user);
-  //   })
-  //   .catch((error: Error) => {
-  //     response.send(error.message);
-  //   })
-});
-
 erzhaler.get('/check-username/:username', (request, response) => {
   const { username } = request.params;
-  accountService.checkUsernameInDB(username)
+  accountService.checkUsernameAvailable(username)
     .then((usernameAvailable: any) => {
       console.log(usernameAvailable);
       response.send(usernameAvailable);
     })
-    .catch((e: Error) => console.error(e.stack));
+    .catch((error: Error) => response.send(error.message));
 });
 
 erzhaler.listen(port, () => {
