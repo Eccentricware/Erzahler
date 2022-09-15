31d9bb2da3b91fbc7861986ad2a8995425d20455
Joseph Peck <joe.r.peck@gmail.com>
2022-09-15 13:03:52 -0700

    I CONTROL THE CONTROLLER!

--- erzahler/server/controller.ts
+++ erzahler/server/controller.ts
@@ -80,7 +80,7 @@ erzhaler.post('/add-provider', (request, response) => {
 erzhaler.post('/new-game', (request, response) => {
   gameService.newGame(request.body.gameData, <string>request.body.idToken)
     .then((result: any) => {
-      response.send({success: 'Success'});
+      response.send(result);
     })
     .catch((error: Error) => {
       response.send({error: error.message});
