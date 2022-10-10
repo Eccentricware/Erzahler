91c195f7581f01d16fc968be1c3af0f4827f7882
Joseph Peck <joe.r.peck@gmail.com>
2022-10-10 12:54:18 -0700

    Find games now accounts for user not-logged in

--- erzahler/database/queries/game/get-games-query.ts
+++ erzahler/database/queries/game/get-games-query.ts
@@ -3,15 +3,40 @@ export const getGamesQuery = `
     g.game_name,
     g.time_created,
     g.game_status,
-    g.concurrent_games_limit,
-    g.private_game,
-    g.blind_administrators,
     g.deadline_type,
-    g.fun_min,
-    g.fun_max,
-    g.skill_min,
-    g.skill_max
+    g.orders_day,
+    g.orders_time AT TIME ZONE $1 orders_time,
+    g.orders_span,
+    g.retreats_day,
+    g.retreats_time AT TIME ZONE $1 retreats_time,
+    g.retreats_span,
+    g.adjustments_day,
+    g.adjustments_time AT TIME ZONE $1 adjustments_time,
+    g.adjustments_span,
+    g.nominations_day,
+    g.nominations_time AT TIME ZONE $1 nominations_time,
+    g.nominations_span,
+    g.votes_day,
+    g.votes_time AT TIME ZONE $1 votes_time,
+    g.votes_span,
+    (
+      SELECT COUNT(*)
+      FROM countries c
+      INNER JOIN games g
+      ON g.game_id = c.game_id
+      WHERE c.rank != 'n'
+      GROUP BY g.game_id
+      LIMIT 1
+    ) as countries,
+    (
+      SELECT COUNT (*)
+      FROM assignments a
+      INNER JOIN games g
+      ON g.game_id = a.game_id
+      WHERE a.assignment_type IN ('registered', 'assigned')
+      GROUP BY g.game_id
+      LIMIT 1
+    ) as players
   FROM games g
-  --WHERE g.game_status in ($1, $2, $3, $4, $5, $6)
   ORDER BY g.time_created;
 `;
