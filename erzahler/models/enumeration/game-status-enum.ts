ef13fc19508a038247efbed93b1bf52cbaff1058
Joseph Peck <joe.r.peck@gmail.com>
2022-10-12 21:38:11 -0700

    Enumerations and time enforcer

---
+++ erzahler/models/enumeration/game-status-enum.ts
@@ -0,0 +1,8 @@
+export enum GameStatus {
+  CANCELLED = 'Cancelled',
+  ENDED = 'Ended',
+  PAUSED = 'Paused',
+  PLAYING = 'Playing',
+  PROCESSING = 'Processing',
+  REGISTRATION = 'Registration'
+}
