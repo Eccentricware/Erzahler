8d781095a6f17bd15898cd43e021c8472af90cf6
Joseph Peck <joe.r.peck@gmail.com>
2022-10-23 16:03:31 -0700

    Routers are modularized

--- erzahler/server/controller.ts
+++ erzahler/server/controller.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { initializeApp } from 'firebase-admin/app';
 import admin from 'firebase-admin';
-import { AccountService } from './services/accountService';
-import { GameService } from './services/gameService';
-import { error } from 'console';
+import { gameRouter } from './routes/game-router';
+import { userRouter } from './routes/user-router';
 const serviceAccount = require('/home/ubox/personal/blitzkarte/Erzahler/erzahler/secrets/erzahler-e66cd-firebase-adminsdk-zgsbb-a50c7851d5.json');
 
 const erzhaler = express();
@@ -16,129 +15,13 @@ erzhaler.use(bodyParser.json({limit: '5mb'}));
 initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
-const accountService = new AccountService();
-const gameService = new GameService();
+erzhaler.use('/games', gameRouter);
+erzhaler.use('/user', userRouter);
 
 erzhaler.get('/check-status', (request, response) => {
   response.send(true);
 });
 
-erzhaler.post('/register-user', (request, response) => {
-  let { idToken, username } = request.body;
-
-  accountService.attemptAddUserToDatabase(idToken, username)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      response.send(error.message);
-    });
-});
-
-erzhaler.get('/check-username/:username', (request, response) => {
-  const { username } = request.params;
-  accountService.checkUsernameAvailable(username)
-    .then((usernameAvailable: any) => {
-      response.send(usernameAvailable);
-    })
-    .catch((error: Error) => response.send(error.message));
-});
-
-erzhaler.put('/update-user-settings', (request, response) => {
-  const idToken = request.headers.idtoken;
-  const data = request.body;
-
-  accountService.updateUserSettings(<string>idToken, data)
-    .then(() => {
-      response.send({success: true});
-    })
-    .catch((error: Error) => {
-      response.send({error: 'Update User Profile Controller Error: ' + error.message});
-    });
-})
-
-erzhaler.get('/check-game-name/:gameName', (request, response) => {
-  const { gameName } = request.params;
-  gameService.checkGameNameAvailability(gameName)
-    .then((gameNameAvailable: boolean) => {
-      response.send(gameNameAvailable);
-    })
-    .catch((error: Error) => response.send('Game availability check error: ' + error.message))
-});
-
-erzhaler.get('/get-user-profile', (request, response) => {
-  const idToken: string = <string>request.headers.idtoken;
-
-  accountService.getUserProfile(idToken)
-    .then((userProfile: any) => {
-      response.send(userProfile);
-    })
-    .catch((error: Error) => {
-      response.send(error.message);
-    })
-});
-
-erzhaler.post('/add-provider', (request, response) => {
-  const { idToken, username } = request.body;
-
-  accountService.addProvider(idToken, username)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      response.send(error.message);
-    });
-});
-
-erzhaler.post('/new-game', (request, response) => {
-  gameService.newGame(request.body.gameData, <string>request.body.idToken)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      response.send({error: error.message});
-    });
-});
-
-erzhaler.put('/update-game', (request, response) => {
-  const idToken = <string>request.headers.idtoken;
-  const gameData = request.body.gameData;
-
-  gameService.updateGameSettings(idToken, gameData)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      response.send({error: error.message});
-    });
-});
-
-erzhaler.get('/game-details/:gameId', (request, response) => {
-  const idToken: any = request.headers.idtoken;
-  const gameId: number = Number(request.params.gameId);
-
-  gameService.getGameData(idToken, gameId)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      console
-      response.send('Get game data error: ' + error.message);
-    });
-});
-
-erzhaler.get('/find-games', (request, response) => {
-  const idToken: any = request.headers.idtoken;
-
-  gameService.findGames(idToken)
-    .then((result: any) => {
-      response.send(result);
-    })
-    .catch((error: Error) => {
-      response.send('Game find error: ' + error.message);
-    });
-});
 
 erzhaler.listen(port, () => {
   console.log(`Erzhaler is running on port ${port}`);
---
+++ erzahler/server/routes/user-router.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import { AccountService } from "../services/accountService";
+
+export const userRouter = express.Router();
+const accountService = new AccountService();
+
+userRouter.get('/check-username/:username', (request, response) => {
+  const { username } = request.params;
+  accountService.checkUsernameAvailable(username)
+    .then((usernameAvailable: any) => {
+      response.send(usernameAvailable);
+    })
+    .catch((error: Error) => response.send(error.message));
+});
+
+userRouter.get('/profile', (request, response) => {
+  const idToken: string = <string>request.headers.idtoken;
+
+  accountService.getUserProfile(idToken)
+    .then((userProfile: any) => {
+      response.send(userProfile);
+    })
+    .catch((error: Error) => {
+      response.send(error.message);
+    })
+});
+
+userRouter.post('/register', (request, response) => {
+  let { idToken, username } = request.body;
+
+  accountService.attemptAddUserToDatabase(idToken, username)
+    .then((result: any) => {
+      response.send(result);
+    })
+    .catch((error: Error) => {
+      response.send(error.message);
+    });
+});
+
+userRouter.post('/add-provider', (request, response) => {
+  const { idToken, username } = request.body;
+
+  accountService.addProvider(idToken, username)
+    .then((result: any) => {
+      response.send(result);
+    })
+    .catch((error: Error) => {
+      response.send(error.message);
+    });
+});
+
+userRouter.put('/update-settings', (request, response) => {
+  const idToken = request.headers.idtoken;
+  const data = request.body;
+
+  accountService.updateUserSettings(<string>idToken, data)
+    .then(() => {
+      response.send({success: true});
+    })
+    .catch((error: Error) => {
+      response.send({error: 'Update User Profile Controller Error: ' + error.message});
+    });
+});
