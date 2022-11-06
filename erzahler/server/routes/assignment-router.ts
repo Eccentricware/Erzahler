e2ec371de5b324b70a0d37af9517f3d63ea0cbc9
Joseph Peck <joe.r.peck@gmail.com>
2022-11-05 22:33:25 -0700

    Assignment scheduler query

--- erzahler/server/routes/assignment-router.ts
+++ erzahler/server/routes/assignment-router.ts
@@ -4,6 +4,19 @@ import { AssignmentService } from "../services/assignmentService";
 export const assignmentRouter = express.Router();
 const assignmentService = new AssignmentService();
 
+assignmentRouter.get(`/:gameId`, (request, response) => {
+  const idToken = <string>request.headers.idtoken;
+  const gameId = Number(request.params.gameId);
+
+  assignmentService.getGameAssignments(idToken, gameId)
+    .then((result: any) => {
+      response.send(result);
+    })
+    .catch((error: Error) => {
+      response.send({error: error.message});
+    });
+})
+
 assignmentRouter.post('/register', (request, response) => {
   const idToken = <string>request.headers.idtoken;
   const gameId = Number(request.body.gameId);
--- erzahler/server/services/assignmentService.ts
+++ erzahler/server/services/assignmentService.ts
@@ -1,14 +1,58 @@
-import { Pool, } from "pg";
+import { Pool, QueryResult, } from "pg";
 import { getPlayerRegistrationStatus } from "../../database/queries/assignments/get-player-registration-status";
 import { registerUserQuery } from "../../database/queries/assignments/register-user-query";
 import { unregisterUserQuery } from "../../database/queries/assignments/unregister-user-query";
 import { updateUserAssignmentQuery } from "../../database/queries/assignments/update-user-assignment-query";
+import { getAssignmentsQuery } from "../../database/queries/game/get-assignments-query";
+import { getRegisteredPlayersQuery } from "../../database/queries/game/get-registered-players-query";
 import { AssignmentStatus } from "../../models/enumeration/assignment-status-enum";
+import { AssignmentDataObject } from "../../models/objects/assignment-data-object";
 import { victorCredentials } from "../../secrets/dbCredentials";
 import { AccountService } from "./accountService";
+import { FormattingService } from "./formattingService";
 
 export class AssignmentService {
   user: any = undefined;
+
+  async getGameAssignments(idToken: string, gameId: number): Promise<any> {
+    const accountService: AccountService = new AccountService();
+    const formattingService: FormattingService = new FormattingService();
+    const pool: Pool = new Pool(victorCredentials);
+    let userId = 0;
+
+    if (idToken) {
+      this.user = await accountService.getUserProfile(idToken);
+      if (!this.user.error) {
+        userId = this.user.userId;
+      }
+    }
+
+    const assignments: any = await pool.query(getAssignmentsQuery, [gameId, userId])
+      .then((assignmentDataResults: QueryResult<any>) => {
+        return assignmentDataResults.rows.map((assignment: any) => formattingService.convertKeysSnakeToCamel(assignment));
+      })
+      .catch((error: Error) => console.log('Get Assignment Data Results Error: ' + error.message));
+
+    const registeredUsers: any = await pool.query(getRegisteredPlayersQuery, [gameId])
+      .then((registeredUserResults: QueryResult<any>) => {
+        return registeredUserResults.rows.map((player: any) => formattingService.convertKeysSnakeToCamel(player));
+      })
+      .catch((error: Error) => console.log('Get Registered Player Data Results Error: ' + error.message));
+
+    const userStatus: any = await pool.query(getPlayerRegistrationStatus, [gameId, userId])
+      .then((playerRegistrationResults: QueryResult<any>) => {
+        return playerRegistrationResults.rows.map((registrationType: any) => formattingService.convertKeysSnakeToCamel(registrationType));
+      })
+      .catch((error: Error) => console.log('Get Player Registration Types Results Error: ' + error.message));
+
+    const assignmentData: AssignmentDataObject = {
+      assignments: assignments,
+      registrants: registeredUsers,
+      userStatus: userStatus
+    };
+
+    return assignmentData;
+  }
 
   async addUserAssignment(idToken: string, gameId: number, assignmentType: string) {
     const accountService: AccountService = new AccountService();
