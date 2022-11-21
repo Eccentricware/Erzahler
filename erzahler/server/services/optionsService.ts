651fd1317f2beb0e61d55976b5883778a7c35a93
Joseph Peck <joe.r.peck@gmail.com>
2022-11-20 21:23:30 -0800

    Placeholder for independent unit movement

--- erzahler/server/services/optionsService.ts
+++ erzahler/server/services/optionsService.ts
@@ -1,5 +1,13 @@
 export class OptionsService {
-  saveOptionsByGameId(gameId: number) {
+  async saveOptionsByGameId(gameId: number): Promise<void> {
+    // Need a query to find the latest resolved turn for the state of the world
+  }
 
+  async processHolds(): Promise<void> {
+    // Fleets can't hold in the fall, UNLESS THEY HAVE POWER!!!
+  }
+
+  async processMovementStandard(): Promise<void> {
+    // Standard is not convoyed
   }
 }
