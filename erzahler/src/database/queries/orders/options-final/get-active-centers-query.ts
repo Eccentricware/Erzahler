0318913e33e2a59e19b3471c07253154684c2f86
Joseph Peck <joe.r.peck@gmail.com>
2023-03-10 13:45:48 -0800

    Get Active Country Centers updated

--- erzahler/src/database/queries/orders/options-final/get-active-centers-query.ts
+++ erzahler/src/database/queries/orders/options-final/get-active-centers-query.ts
@@ -1,22 +1,12 @@
 export const getActiveCountryCenters = `
-  WITH last_province_history_index AS (
-    SELECT ph.province_id,
-      ph.turn_id,
-      MAX(t.turn_number) AS turn_number
-    FROM province_histories ph
-    INNER JOIN turns t ON t.turn_id = ph.turn_id
-    WHERE t.turn_number <= $1
-    GROUP BY ph.province_id,
-      ph.turn_id
-  )
   SELECT p.province_name,
     n.node_id,
     n.loc
-  FROM last_province_history_index lphi
-  INNER JOIN province_histories ph ON ph.province_id = lphi.province_id AND ph.turn_id = lphi.turn_id
+  FROM get_last_province_history($1, $2) lph
+  INNER JOIN province_histories ph ON ph.province_id = lph.province_id AND ph.turn_id = lph.turn_id
   INNER JOIN provinces p ON p.province_id = ph.province_id
   INNER JOIN nodes n ON n.province_id = p.province_id
-  WHERE ph.controller_id = $2
+  WHERE ph.controller_id = $3
     AND ph.province_status = 'active'
     AND n.node_type = 'land';
 `;
