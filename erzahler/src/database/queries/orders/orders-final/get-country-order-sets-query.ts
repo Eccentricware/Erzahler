4a1f58fd53a0fb5228818a653a6c07722f18b735
Joseph Peck <joe.r.peck@gmail.com>
2023-03-11 14:17:08 -0800

    Get Country Order Set refactored

--- erzahler/src/database/queries/orders/orders-final/get-country-order-sets-query.ts
+++ erzahler/src/database/queries/orders/orders-final/get-country-order-sets-query.ts
@@ -6,12 +6,12 @@ export const getCountryOrderSets = `
     os.order_set_id,
     ch.adjustments
   FROM country_histories ch
+  INNER JOIN get_last_country_history($1, $2) lch
+    ON lch.country_id = ch.country_id AND lch.turn_id = ch.turn_id
   INNER JOIN countries c ON c.country_id = ch.country_id
   INNER JOIN order_sets os ON os.country_id = c.country_id
   INNER JOIN turns t ON t.turn_id = os.turn_id
-  WHERE c.game_id = $1
-    AND ch.turn_id = $2
-    AND c.country_id = $3
+  WHERE CASE WHEN $3 = 0 THEN true ELSE c.country_id = $3 END
     AND t.turn_status IN ('Pending', 'Preliminary')
   ORDER BY ch.turn_id DESC;
 `;
