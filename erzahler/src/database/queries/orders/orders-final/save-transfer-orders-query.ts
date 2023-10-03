e07940f688e5498c74c936bb22b44af542119ec3
Joseph Peck <joe.r.peck@gmail.com>
2023-10-03 09:55:40 -0700

    Added ui_row to orders_transfers. Related object definition refining

--- erzahler/src/database/queries/orders/orders-final/save-transfer-orders-query.ts
+++ erzahler/src/database/queries/orders/orders-final/save-transfer-orders-query.ts
@@ -1,13 +1,3 @@
-export const saveTransferOrdersQuery = `
-  UPDATE order_sets
-  SET submission_time = NOW(),
-    default_orders = false,
-    tech_partner_id = $1,
-    build_transfer_recipients = $2,
-    build_transfer_tuples = $3
-  WHERE order_set_id = $4;
-`;
-
 export const updateTechTransferOrdersQuery = `
   UPDATE orders_transfers
   SET
