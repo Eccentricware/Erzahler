32105dec956ba83c329afe0ef3acbad9876cda0c
Joseph Peck <joe.r.peck@gmail.com>
2024-01-06 01:20:59 -0800

    Fast returns identification of guest when empty string idToken is presented

--- erzahler/src/server/routes/options-router.ts
+++ erzahler/src/server/routes/options-router.ts
@@ -9,6 +9,11 @@ optionsRouter.get(`/:gameId`, (request, response) => {
   const idToken = <string>request.headers.idtoken;
   const gameId = Number(request.params.gameId);
 
+  if (idToken === '') {
+    response.send({ success: false, error: 'Guests cannot have order options.' });
+    return;
+  }
+
   optionsService.getTurnOptions(idToken, gameId).then((options: OptionsFinal | string) => {
     response.send(options);
   });
