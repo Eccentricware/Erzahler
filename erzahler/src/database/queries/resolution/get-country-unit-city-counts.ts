3f84fae63e3e4e1dc484fbc79492562381909495
Joseph Peck <joe.r.peck@gmail.com>
2024-01-28 15:49:07 -0800

    pgAdmin is helpful

--- erzahler/src/database/queries/resolution/get-country-unit-city-counts.ts
+++ erzahler/src/database/queries/resolution/get-country-unit-city-counts.ts
@@ -1,21 +1,20 @@
 export const getCountryUnitCityCountsQuery = `
   WITH unit_counts AS (
     SELECT u.country_id,
-      COUNT(u.unit_id) AS unit_count
+      COUNT(u.unit_id) AS unit_count,
+    lch.nukes_in_production
     FROM units u
-    INNER JOIN unit_histories uh ON uh.unit_id = u.unit_id
-    INNER JOIN get_last_unit_history($1, $2) luh
-      ON luh.unit_id = uh.unit_id AND luh.turn_id = uh.turn_id
-    WHERE uh.unit_status IN ('Active', 'Retreat')
-    GROUP BY u.country_id
+    INNER JOIN get_last_unit_history($1, $2) luh ON luh.unit_id = u.unit_id
+  INNER JOIN get_last_country_history($1, $2) lch ON lch.country_id = u.country_id
+    WHERE luh.unit_status IN ('Active', 'Retreat')
+    GROUP BY u.country_id,
+    lch.nukes_in_production
   ), city_counts AS (
-    SELECT ph.controller_id,
-      COUNT(ph.province_id) AS city_count
-    FROM province_histories ph
-    INNER JOIN get_last_province_history($1, $2) lph
-      ON lph.province_id = ph.province_id AND lph.turn_id = ph.turn_id
-    WHERE ph.province_status = 'active'
-    GROUP BY ph.controller_id
+    SELECT lph.controller_id,
+      COUNT(lph.province_id) AS city_count
+    FROM get_last_province_history($1, $2) lph
+    WHERE lph.province_status = 'active'
+    GROUP BY lph.controller_id
   ), vote_counts AS (
     SELECT lph.controller_id,
       COUNT(lph.province_id) AS vote_count
@@ -36,19 +35,20 @@ export const getCountryUnitCityCountsQuery = `
       lph.controller_id AS occupying_country_id
     FROM get_last_province_history($1, $2) lph
     INNER JOIN provinces p ON p.province_id = lph.province_id
-    WHERE p.city_type = 'capital' AND p.capital_owner_id = lch.controller_id
+    INNER JOIN countries c ON c.country_id = p.capital_owner_id
+    WHERE p.city_type = 'capital' AND p.capital_owner_id = c.country_id
   )
   SELECT c.country_id,
-    uc.unit_count + lch.nukes_in_production AS unit_count,
+    uc.unit_count + uc.nukes_in_production AS unit_count,
     cc.city_count,
-    cc.city_count - uc.unit_count - lch.nukes_in_production AS adjustments,
+    cc.city_count - uc.unit_count - uc.nukes_in_production AS adjustments,
     vc.vote_count,
     oc.occupying_country_id
   FROM countries c
-  INNER JOIN get_last_country_history($1, $2) lch ON lch.country_id = c.country_id
   INNER JOIN unit_counts uc ON uc.country_id = c.country_id
   INNER JOIN city_counts cc ON cc.controller_id = c.country_id
-  INNER JOIN vote_counts vc ON vc.controller_id = c.country_id
-  INNER JOIN occupying_country_id oc ON oc.capital_owner_id = c.country_id
+  LEFT JOIN vote_counts vc ON vc.controller_id = c.country_id
+  LEFT JOIN occupying_country_id oc ON oc.capital_owner_id = c.country_id
+  WHERE c.game_id = $1
   ORDER BY c.country_id;
 `;
