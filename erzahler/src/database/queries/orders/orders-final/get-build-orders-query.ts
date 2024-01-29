326293df92c6623976901d16dc667d62bdfa6ab2
Joseph Peck <joe.r.peck@gmail.com>
2024-01-28 17:27:39 -0800

    Purging yet another base history row from query

--- erzahler/src/database/queries/orders/orders-final/get-build-orders-query.ts
+++ erzahler/src/database/queries/orders/orders-final/get-build-orders-query.ts
@@ -2,8 +2,8 @@ export const getBuildOrdersQuery = `
   SELECT
     c.country_id,
     c.country_name,
-    ch.banked_builds,
-    ch.adjustments builds,
+    lch.banked_builds,
+    lch.adjustments builds,
     CASE
       WHEN lch.adjustments > 0 THEN
         json_agg(
@@ -19,7 +19,7 @@ export const getBuildOrdersQuery = `
         )
       ELSE NULL
     END AS builds,
-    ch.nuke_range,
+    lch.nuke_range,
     os.increase_range
   FROM order_sets os
   LEFT JOIN orders_adjustments bo ON bo.order_set_id = os.order_set_id
@@ -27,15 +27,14 @@ export const getBuildOrdersQuery = `
   LEFT JOIN provinces p ON p.province_id = n.province_id
   LEFT JOIN countries c ON c.country_id = os.country_id
   LEFT JOIN get_last_country_history($1, $2) lch ON lch.country_id = c.country_id
-  LEFT JOIN country_histories ch ON ch.country_id = lch.country_id AND ch.turn_id = lch.turn_id
   WHERE os.turn_id = $3
     AND order_set_type = 'Orders'
     AND CASE WHEN 0 = $4 THEN true ELSE os.country_id = $4 END
   GROUP BY
     c.country_id,
     c.country_name,
-    ch.banked_builds,
-    ch.adjustments,
-    ch.nuke_range,
+    lch.banked_builds,
+    lch.adjustments,
+    lch.nuke_range,
     os.increase_range;
 `;
