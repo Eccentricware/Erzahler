6c184f218d917970bb805e3a1b6d6bfa329309bb
Joseph Peck <joe.r.peck@gmail.com>
2024-05-04 17:01:09 -0700

    Better organized data provided

---
+++ erzahler/src/database/queries/history/get-historic-build-orders-query.ts
@@ -0,0 +1,40 @@
+export const getHistoricBuildOrdersQuery = `
+  SELECT
+    c.country_id,
+    c.country_name,
+    lch.banked_builds,
+    lch.adjustments adjustments,
+    CASE
+      WHEN lch.adjustments > 0 THEN
+        json_agg(
+          json_build_object(
+            'build_number', bo.build_number,
+            'build_type', bo.build_type,
+            'node_id', n.node_id,
+            'node_name', n.node_name,
+            'node_display', n.node_display,
+            'province_name', p.province_name,
+            'loc', n.loc
+          )
+        )
+      ELSE NULL
+    END AS builds,
+    lch.nuke_range,
+    os.increase_range
+  FROM order_sets os
+  LEFT JOIN orders_adjustments bo ON bo.order_set_id = os.order_set_id
+  LEFT JOIN nodes n ON n.node_id = bo.node_id
+  LEFT JOIN provinces p ON p.province_id = n.province_id
+  LEFT JOIN countries c ON c.country_id = os.country_id
+  LEFT JOIN get_last_country_history($1, $2) lch ON lch.country_id = c.country_id
+  WHERE os.turn_id = $3
+    AND order_set_type = 'Orders'
+    AND CASE WHEN 0 = $4 THEN true ELSE os.country_id = $4 END
+  GROUP BY
+    c.country_id,
+    c.country_name,
+    lch.banked_builds,
+    lch.adjustments,
+    lch.nuke_range,
+    os.increase_range;
+`;
--- erzahler/src/database/queries/history/get-historic-turn-query.ts
+++ erzahler/src/database/queries/history/get-historic-turn-query.ts
@@ -10,23 +10,32 @@ export const getHistoricTurnQuery = `
     0 as year_stylized,
     t.deadline,
     json_agg(json_build_object(
-      'countryId', c.country_id,
-      'countryName', c.country_name,
+      'country_id', c.country_id,
+      'country_name', c.country_name,
       'rank', c.rank,
       'username', u.username,
-      'cityCount', lch.city_count,
-      'voteCount', lch.vote_count,
-      'bankedBuilds', lch.banked_builds,
-      'nukeRange', lch.nuke_range,
-      'adjustments', lch.adjustments,
-      'techPartnerName', tpc.country_name,
-      'votes', os.votes,
-      'increaseRange', os.increase_range
-    )) as surviving_countries
+      'city_count_start', lchs.city_count,
+      'city_count_result', lchr.city_count,
+      'unit_count_start', lchs.unit_count,
+      'unit_count_result', lchr.unit_count,
+      'vote_count_start', lchs.vote_count,
+      'vote_count_result', lchr.vote_count,
+      'banked_builds_start', lchs.banked_builds,
+      'banked_builds_result', lchr.banked_builds,
+      'nuke_range_start', lchs.nuke_range,
+      'nuke_range_result', lchr.nuke_range,
+      'adjustments_start', lchs.adjustments,
+      'adjustments_result', lchr.adjustments,
+      'tech_partner_id', os.tech_partner_id,
+      'tech_partner_name', tpc.country_name,
+      'vote_orders', os.votes,
+      'increase_range_orders', os.increase_range
+    )) as historic_countries
   FROM turns t
   INNER JOIN games g ON g.game_id = t.game_id
   INNER JOIN countries c ON c.game_id = g.game_id
-  INNER JOIN get_last_country_history($1, $2) lch ON lch.country_id = c.country_id
+  INNER JOIN get_last_country_history($1, $2) lchr ON lchr.country_id = c.country_id
+  INNER JOIN get_last_country_history($1, $3) lchs ON lchs.country_id = c.country_id
   LEFT JOIN order_sets os ON os.turn_id = t.turn_id AND os.country_id = c.country_id
   LEFT JOIN countries tpc ON tpc.country_id = os.tech_partner_id
   LEFT JOIN assignments a ON a.country_id = c.country_id AND (
@@ -37,7 +46,6 @@ export const getHistoricTurnQuery = `
   WHERE t.game_id = $1
     AND t.turn_number = $2
     AND t.turn_status IN ('Resolved', 'Final')
-    --AND lch.country_status IN ('Active', 'Civil Disorder')
     AND c.rank != 'n'
   GROUP BY t.game_id,
     t.turn_id,
